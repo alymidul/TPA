5fd48e508221207ad60519d8be780526f1392024
Sabrina Cristan <sabrinacristan@gmail.com>
2022-11-24 21:09:29 -0300

    Aula 2 adicionada

---
+++ aula02/src/aula02/exercicio02.java
@@ -0,0 +1,17 @@
+package aula02;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+		int valor1,valor2,result;
+		
+		 valor1 = 100;
+		 valor2 = 300;
+		 result= (valor1 + valor2 );
+			System.out.println("A soma dos valores " +valor1+ " e " +valor2+ " será igual a " +result);
+					
+				 
+	
+	}
+
+}
---
+++ aula02/src/aula02/exercicio03.java
@@ -0,0 +1,92 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		Scanner leitura = new Scanner(System.in);
+		
+		int var1,var2, result;
+		
+		System.out.println("Digite o 1° valor:");
+		var1 = leitura.nextInt();
+		System.out.println("Digite o 2° valor:");
+		var2 = leitura.nextInt();
+		result = (var1+var2);
+		System.out.println("o resultado esperado será: ");
+		System.out.println(result);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
